315a91c444a823f18d4411ed3ffe8851f339b9b2
Virgil Mihai <virgil.mihai@live.com>
2016-05-29 06:39:54 +0300

    First setup of laravel project

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('page');
+
+@section('content');
+	<div class="title">Hello {{ $name }}</div>
+@stop;
---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,5 @@
+@extends('page');
+
+@section('content');
+<div class="title">Contact Me!</div>
+@stop;
